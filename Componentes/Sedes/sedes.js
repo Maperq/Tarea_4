20a7b8b1a5481ebc56587ffe20b17b2b6b46d9df
riclop335 <riclop335@gmail.com>
2023-03-12 20:32:32 -0500

    Subir proyecto

---
+++ Componentes/Sedes/sedes.js
@@ -0,0 +1,22 @@
+
+
+
+function lim_sedes()
+{   
+    let formulario = document.getElementById('form_sedes');
+    formulario.reset();
+}
+
+function esc_sedes(){
+   
+    var nombre = document.getElementById("nom_sede").value;
+    var direccion = document.getElementById("dir_sede").value;
+    var telefono = document.getElementById("tel_sede").value;
+    var email = document.getElementById("email_sede").value;
+    
+    var fso = new ActiveXObject("Scripting.FileSystemObject");
+    var fh = fso.OpenTextFile("sedes.txt", 8, false, 0);
+    fh.WriteLine(formulario);
+    fh.Close();
+}
+
